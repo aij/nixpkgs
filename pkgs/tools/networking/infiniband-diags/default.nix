8545509eb9e4412ab751c01424fb7bc1baee5eab
Ivan Jager <aij+git@mrph.org>
2017-12-26 17:37:48 -0600

    infiniband-diags: init at 2.0.0
    (cherry picked from commit 6dfe15185255ed70466313160f8d591c96e503b3)

--- pkgs/tools/networking/infiniband-diags/default.nix
+++ pkgs/tools/networking/infiniband-diags/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchFromGitHub, autoconf, automake, libtool, pkgconfig, rdma-core, glib, opensm }:
+{ stdenv, fetchFromGitHub, autoconf, automake, libtool, pkgconfig, rdma-core,
+  glib, opensm, perl, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "infiniband-diags-${version}";
@@ -11,16 +12,27 @@ stdenv.mkDerivation rec {
     sha256 = "06x8yy3ly1vzraznc9r8pfsal9mjavxzhgrla3q2493j5jz0sx76";
   };
 
-  nativeBuildInputs = [ autoconf automake libtool pkgconfig ];
+  nativeBuildInputs = [ autoconf automake libtool pkgconfig makeWrapper ];
 
-  buildInputs = [ rdma-core glib opensm ];
+  buildInputs = [ rdma-core glib opensm perl ];
 
   preConfigure = ''
     export CFLAGS="-I${opensm}/include/infiniband"
     ./autogen.sh
   '';
 
-  configureFlags = "--with-perl-installdir=\\\${out}/lib/perl";
+  configureFlags = "--with-perl-installdir=\${out}/lib/perl5/site_perl --sbindir=\${out}/bin";
+
+  postInstall = ''
+    rmdir $out/var/run $out/var
+  '';
+
+  postFixup = ''
+    for pls in $out/bin/{ibfindnodesusing.pl,ibidsverify.pl}; do
+      echo "wrapping $pls"
+      wrapProgram $pls --prefix PERL5LIB : "$out/lib/perl5/site_perl"
+    done
+  '';
 
   meta = with stdenv.lib; {
     description = "Utilities designed to help configure, debug, and maintain infiniband fabrics";
